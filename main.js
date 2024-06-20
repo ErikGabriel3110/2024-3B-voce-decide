c7d208fa9f83eb344b8c94e3aec0bb71bad0bbb6
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2024-06-20 08:28:50 -0300

    Aula 01

---
+++ main.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternatias');
+const caixaResultado = document.querySelector('.caixa-Resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Enunciado 01",
+        Alternativas:[
+            "alternativa 01",
+            "alternativa 02"
+        ]
+    },
+    {
+        enunciado: "Enunciado 02",
+        Alternativas:[
+            "alternativa 01",
+            "alternativa 02"
+        ]
+    },
+    {
+        enunciado: "Enunciado 03",
+        Alternativas:[
+            "alternativa 01",
+            "alternativa 02"
+        ]
+    },
+    {
+        enunciado: "Enunciado 04",
+        Alternativas:[
+            "alternativa 01",
+            "alternativa 02"
+        ]
+    },
+    {
+        enunciado: "Enunciado 05",
+        Alternativas:[
+            "alternativa 01",
+            "alternativa 02"
+        ]
+    },
+]
