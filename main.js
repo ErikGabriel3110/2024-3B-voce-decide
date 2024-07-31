7b28c24bc8529f0f4d53fd54e6d960e0a2b1de07
Erik Gabriel De lima Rodrigues <106687673+ErikGabriel3110@users.noreply.github.com>
2024-07-31 08:40:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,56 +8,103 @@ const perguntas = [
     {
         enunciado: "Enunciado 01",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 01",
+                afirmacao: "resultado 01"
+            },
+            {
+                texto: "alternativa 02",
+                afirmacao: "resultado 02"
+            },
         ]
     },
     {
         enunciado: "Enunciado 02",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado 03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado 04"
+            },
         ]
     },
     {
         enunciado: "Enunciado 03",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            },
         ]
     },
     {
         enunciado: "Enunciado 04",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            },
         ]
     },
     {
         enunciado: "Enunciado 05",
         alternativas: [
-            "alternativa 01",
-            "alternativa 02"
+            {
+                texto: "alternativa 09",
+                afirmacao: "resultado 09"
+            },
+            {
+                texto: "alternativa 10",
+                afirmacao: "resultado 10"
+            },
         ]
     },
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostrarPerguntas() {
+    if (atual >= perguntas.length) {
+        mostrarResultado();
+        return
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostrarAlternativas();
 }
 
 function mostrarAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas){
-         const botaoAlternativas = document.createElement("button");
-         botaoAlternativas.textContent = alternativa;
-         caixaAlternativas.appendChild(botaoAlternativas);
-
-         }
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendchild(botaoAlternativas);
+    }
 }
 
-mostrarPerguntas();
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opecaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostrarPerguntas();
+}
+
+function mostrarResultado(){
+    caixaPerguntas.textContent = " Em 2049 ..."
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
